1714902dcfb3f000142fd71f69698ee9496a7483
SunCat <suncat.game@ya.ru>
2025-04-27 22:42:36 +0300

    update godot_ap

--- godot_ap/ui/SettingsTab.gd
+++ godot_ap/ui/SettingsTab.gd
@@ -1,13 +1,21 @@
 extends MarginContainer
 
+@export_group("Nodes")
+@export_subgroup("Tracker")
+@export var tracker_tab: Container
+@export var track_verbose: CheckBox
+@export var track_hide_completed_map: CheckBox
+
+
 func _ready():
-	var track_verbose: CheckBox = find_child("TrackVerbose")
-	if track_verbose:
-		track_verbose.toggled.connect(func(state: bool):
-			Archipelago.config.verbose_trackerpack = state)
-		track_verbose.button_pressed = Archipelago.config.verbose_trackerpack
-	var track_hide_completed_map: CheckBox = find_child("TrackHideFinishedMap")
-	if track_hide_completed_map:
-		track_hide_completed_map.toggled.connect(func(state: bool):
-			Archipelago.config.hide_finished_map_squares = state)
-		track_hide_completed_map.button_pressed = Archipelago.config.hide_finished_map_squares
+	if Archipelago.AP_ALLOW_TRACKERPACKS:
+		if track_verbose:
+			track_verbose.toggled.connect(func(state: bool):
+				Archipelago.config.verbose_trackerpack = state)
+			track_verbose.button_pressed = Archipelago.config.verbose_trackerpack
+		if track_hide_completed_map:
+			track_hide_completed_map.toggled.connect(func(state: bool):
+				Archipelago.config.hide_finished_map_squares = state)
+			track_hide_completed_map.button_pressed = Archipelago.config.hide_finished_map_squares
+	else:
+		tracker_tab.queue_free()
